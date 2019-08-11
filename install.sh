b511f20bfc0c3b075642b042ddf118d98c1f4ce9
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2019-08-11 22:40:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install neovim
 echo "alias vim=\"nvim\"\nalias vi=\"nvim\"" >> .zshrc
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp -Rf .config ~/
 ln -s ~/.vim ~/.config
