7b26c6300e10c5ea843717db52e9c305ce1d66b5
Florian <flbeaumo@student.42.fr>
2019-08-15 17:17:31 +0200

    simplify installer

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install neovim
-echo "alias vim=\"nvim\"\nalias vi=\"nvim\"" >> .zshrc
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp -Rf .config ~/
-ln -s ~/.vim ~/.config
+cat .config/nvim/init.vim > ~/.vimrc
