61678647e8e05469839bd77fca0ecb585458afca
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2019-08-15 17:51:08 +0200

    Update install.sh for vim 🐳

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cat .config/nvim/init.vim > ~/.vimrc
